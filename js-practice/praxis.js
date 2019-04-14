2297317b0fede57beb5d941dd9b0bc0a768a3535
Otha Hernandez <othahernandez@Marx.home>
2019-04-14 17:43:24 -0400

    Stupid shell script

--- js-practice/praxis.js
+++ js-practice/praxis.js
@@ -1,15 +1,4 @@
-// var one = [1,2,3,4,5];
-// var two = [,6,7,8,9,10];
 
-
-// function first() {
-// 	for ( let i = 0; i < one.length; i++) {
-// 		console.log(one[i]);
-
-// 	}
-// }
-
-// first(one)
 
 // Using a callback to store a function so
 // I can call it in the set interval function.
@@ -40,45 +29,6 @@
 
 
 
-// 
-// function currentLine(line) {
-//   if (line.length === 0) {
-//     return "The line is currently empty.";
-//   }
-
-//   const numbersAndNames = []; // const cant be reassigned
-
-//   for (let i = 0, l = line.length; i < l; i++) {
-//     numbersAndNames.push(`${i + 1}. ${line[i]}`);
-//   }
-
-//   return `The line is currently: ${numbersAndNames.join(', ')}`;
-// }
-
-
-
-
-
-// function nowServing(line) {
-//   if (line.length === 0) {
-//     return "There is nobody waiting to be served!";
-//   }
-
-//   return `Currently serving ${line.shift()}.`;
-// }
-
-// function takeANumber(line, name) {
-//   line.push(name);
-
-//   return `Welcome, ${name}. You are number ${line.length} in line.`;
-// }
-
-
-
-
-
-
-
 setInterval(function() {
 	console.log("☭ Communism ☭");
 }, 500);
@@ -92,6 +42,14 @@ setInterval(function() {
 setInterval(function() {
 	console.log("☭☭☭☭☭☭☭☭☭☭");
 }, 502);
+
+setInterval(function() {
+	console.log("~~~~~~~~~~~~");
+}, 503);
+
+setInterval(function() {
+	console.log("~~~~~~~~~~~~");
+}, 504);
 
 
 
